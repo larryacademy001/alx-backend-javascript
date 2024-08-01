b6f27fe32e267f94b59872668658196d9a6fa184
Larry Academy <olanrewajuawoyemi@gmail.com>
2024-08-01 02:50:57 +0100

    Push All

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,17 @@
+/**
+ * Filter - Retrieves students in a given city.
+ * @param {{
+ *   id: Number,
+ *   firstName: String,
+ *   location: String
+ * }[]} students - The list of students.
+ * @param {String} city - The City.
+ * @author Olanrewaju Awoyemi <https://github.com/larryacademy001>
+ * @returns
+ */
+export default function getStudentsByLocation(students, city) {
+  if (students instanceof Array) {
+    return students.filter((student) => student.location === city);
+  }
+  return [];
+}
---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author Olanrewaju Awoyemi <https://github.com/larryacademy001>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
