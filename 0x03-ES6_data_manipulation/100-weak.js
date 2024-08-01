798909e4c6b6f0bcea7642fe26613aa1470b490a
Larry Academy <olanrewajuawoyemi@gmail.com>
2024-08-01 03:02:08 +0100

    update

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -14,7 +14,7 @@ const MAX_ENDPOINT_CALLS = 5;
  *   protocol: String,
  *   name: String,
  * }} endpoint - The endpoint to make a request to.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author Olanrewaju Awoyemi <https://github.com/larryacademy001>
  */
 export function queryAPI(endpoint) {
   if (!weakMap.has(endpoint)) {
