8982cd76e63662ffd17420b8aa1a307c7b524a6c
Olanrewaju Awoyemi <olanrewajuawoyemi@gmail.com>
2024-09-19 05:21:18 +0100

    Done 0x05. NodeJS Basics

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Olanrewaju Awoyemi <https://github.com/larryacademy001>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
